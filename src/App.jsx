e65be4c6b9ac25607f5accff28dd728f6f0be04f
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-09-06 04:23:39 +0300

    feat: add About page
    - Add AboutUs component with header, hero image, Our Story and What we do sections
    - Apply Tailwind styling for headings, paragraphs, and spacing
    - Wire up /about route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import HomePage from "./pages/Home";
-
+import AboutPage from "./pages/AboutUs";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-      </Routes>
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>      
     </BrowserRouter>
   );
 }
