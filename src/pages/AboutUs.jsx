f3ce37d949012067c0b30591fc7d8b46aef03830
Omar Yousry <152629913+omar23py@users.noreply.github.com>
2026-09-06 04:51:38 +0300

    style(AboutUs): improve layout and typography for better readability

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,8 +2,8 @@ import aboutImage from "../assets/images/about.png";
 
 const AboutUs = () => {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center m-3">
-      <header className="text-center my-12">
+    <main className="min-h-screen flex flex-col justify-center items-center m-3 md:m-5">
+      <header className="text-center my-12 md:my-20">
         <h1 className="text-5xl mb-2">Our company</h1>
         <p className="text-text/70">Guided by principles, driven by results</p>
       </header>
@@ -11,12 +11,12 @@ const AboutUs = () => {
       <img
         src={aboutImage}
         alt="office"
-        className="w-full h-auto my-4 max-w-7xl rounded-lg"
+        className="w-full h-auto my-6 max-w-7xl rounded-lg"
       />
 
-      <div className="flex flex-col gap-10 max-w-7xl">
+      <div className="flex flex-col gap-10 max-w-7xl ">
         <section>
-          <h2 className="text-2xl font-semibold mb-3">Our Story</h2>
+          <h2 className="text-2xl font-normal mb-3 lg:text-4xl">Our Story</h2>
           <div className="space-y-4">
             <p className="text-text/60">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
@@ -31,7 +31,7 @@ const AboutUs = () => {
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-3">What we do</h2>
+          <h2 className="text-2xl font-normal mb-3 lg:text-4xl">What we do</h2>
           <div className="space-y-4">
             <p className="text-text/60">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
